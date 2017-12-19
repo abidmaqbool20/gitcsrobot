9f5022cf2f3b6297c9f3994f5c31f0955c7a18c5
abidmaqbool20 <abidmaqbool20@gmail.com>
2017-12-19 20:44:50 +0500

    All Files First Upload
    Project first uploaded. All files

---
+++ departments.php
@@ -0,0 +1,179 @@
+<?php include("includes/header.php");  ?>
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Organization Departments
+        
+      </h1>
+      <ol class="breadcrumb">
+        
+        <li><a href="#"><i class="fa fa-building-o"></i> Organization</a></li>
+        <li class="active">Departments</li>
+      </ol>
+    </section><br>
+    <section class="content-header top-action-bar" style="">
+      <div class="row">
+        <div class="col-md-10">
+           <p><i class="icon fa fa-info-circle"></i>&nbsp;Manage all the department details and the department hierarchy in your organization.</p>
+        </div>
+        <div class="col-md-2">
+          <button class="btn btn-success"><i class="fa fa-plus"></i>&nbsp; Add Department</button>
+          <a href="#" class="top-info-btn"><i class="icon fa fa-info-circle"></i></a>
+        </div>
+      </div>
+    </section>
+    <section class="content-header content-info-alert">
+      <div class="callout callout-info">
+        <h4><i class="icon fa fa-info-circle"></i>&nbsp;&nbsp;Manage Departments!
+          <button type="button" class="close close-info" >
+          <i class="fa fa-times"></i>
+         </button>
+      </h4>
+        <p>Manage all the department details and the department hierarchy in your organization.</p>
+
+    </div>
+
+  </section>
+    
+    <section class="content">
+      <div class="row">
+        
+        
+        <div class="col-md-4">
+          
+          <div class="box box-widget widget-user">
+           
+            <div class="widget-user-header bg-aqua-active">
+              <h3 class="widget-user-username"><b>IT Department</b></h3>
+              <h5 class="widget-user-desc">Gulam Murtaza </h5>
+            </div>
+            <div class="widget-user-image">
+              <img class="img-circle" src="assets/images/user.png" alt="User Avatar">
+            </div>
+            <div class="box-footer">
+              <div class="row">
+                <div class="col-sm-4 border-right">
+                  <div class="description-block">
+                    <h5 class="description-header">3,200</h5>
+                    <span class="description-text">SALES</span>
+                  </div>
+                 
+                </div>
+               
+                <div class="col-sm-4 border-right">
+                  <div class="description-block">
+                    <h5 class="description-header">13,000</h5>
+                    <span class="description-text">FOLLOWERS</span>
+                  </div>
+                  
+                </div>
+                
+                <div class="col-sm-4">
+                  <div class="description-block">
+                    <h5 class="description-header">35</h5>
+                    <span class="description-text">PRODUCTS</span>
+                  </div>
+                 
+                </div>
+               
+              </div>
+            
+            </div>
+          </div>
+      
+        </div>
+       <div class="col-md-4">
+          
+          <div class="box box-widget widget-user">
+           
+            <div class="widget-user-header bg-success">
+              <h3 class="widget-user-username"><b>IT Department</b></h3>
+              <h5 class="widget-user-desc">Gulam Murtaza </h5>
+            </div>
+            <div class="widget-user-image">
+              <img class="img-circle" src="assets/images/user.png" alt="User Avatar">
+            </div>
+            <div class="box-footer">
+              <div class="row">
+                <div class="col-sm-4 border-right">
+                  <div class="description-block">
+                    <h5 class="description-header">3,200</h5>
+                    <span class="description-text">SALES</span>
+                  </div>
+                 
+                </div>
+               
+                <div class="col-sm-4 border-right">
+                  <div class="description-block">
+                    <h5 class="description-header">13,000</h5>
+                    <span class="description-text">FOLLOWERS</span>
+                  </div>
+                  
+                </div>
+                
+                <div class="col-sm-4">
+                  <div class="description-block">
+                    <h5 class="description-header">35</h5>
+                    <span class="description-text">PRODUCTS</span>
+                  </div>
+                 
+                </div>
+               
+              </div>
+            
+            </div>
+          </div>
+      
+        </div>
+       <div class="col-md-4">
+          
+          <div class="box box-widget widget-user">
+           
+            <div class="widget-user-header bg-warning">
+              <h3 class="widget-user-username"><b>IT Department</b></h3>
+              <h5 class="widget-user-desc">Gulam Murtaza </h5>
+            </div>
+            <div class="widget-user-image">
+              <img class="img-circle" src="assets/images/user.png" alt="User Avatar">
+            </div>
+            <div class="box-footer">
+              <div class="row">
+                <div class="col-sm-4 border-right">
+                  <div class="description-block">
+                    <h5 class="description-header">3,200</h5>
+                    <span class="description-text">SALES</span>
+                  </div>
+                 
+                </div>
+               
+                <div class="col-sm-4 border-right">
+                  <div class="description-block">
+                    <h5 class="description-header">13,000</h5>
+                    <span class="description-text">FOLLOWERS</span>
+                  </div>
+                  
+                </div>
+                
+                <div class="col-sm-4">
+                  <div class="description-block">
+                    <h5 class="description-header">35</h5>
+                    <span class="description-text">PRODUCTS</span>
+                  </div>
+                 
+                </div>
+               
+              </div>
+            
+            </div>
+          </div>
+      
+        </div>
+       
+      </div>
+     
+      
+    </section>
+ 
+  </div>
+<?php include("includes/footer.php");  ?>
---
+++ locations.php
@@ -0,0 +1,75 @@
+<?php include("includes/header.php");  ?>
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Organization Locations 
+        
+      </h1>
+      <ol class="breadcrumb">
+        
+        <li><a href="#"><i class="fa fa-building-o"></i> Organization</a></li>
+        <li class="active">Locations</li>
+      </ol>
+    </section><br>
+    <section class="content-header top-action-bar" style="">
+      <div class="row">
+        <div class="col-md-10">
+           <p><i class="icon fa fa-info-circle"></i>&nbsp;Create multiple locations for your organization and manage location details</p>
+        </div>
+        <div class="col-md-2">
+          <button class="btn btn-success"><i class="fa fa-plus"></i>&nbsp; Add Location</button>
+          <a href="#" class="top-info-btn"><i class="icon fa fa-info-circle"></i></a>
+        </div>
+      </div>
+    </section>
+    <section class="content-header content-info-alert">
+      <div class="callout callout-info">
+        <h4>
+          <i class="icon fa fa-info-circle"></i>&nbsp;&nbsp;Manage Locations!
+          <button type="button" class="close close-info" > <i class="fa fa-times"></i> </button>
+        </h4>
+        <p>Create multiple locations for your organization and manage location details.</p>
+      </div>
+
+    </section>
+    
+    <section class="content">
+      <div class="row">
+        
+        
+        <div class="col-md-4">
+          
+          <div class="box box-widget widget-user">
+           
+            <div class="widget-user-header designations bg-aqua-active">
+              <h3 class="widget-user-username"><b>Lahore</b></h3>
+              <h5 class="widget-user-desc"><i class="fa fa-map"></i> Address : GOR - 1, Golf Road, Lahore</h5>
+            </div>
+            
+             
+          </div>
+      
+        </div>
+         <div class="col-md-4">
+          
+          <div class="box box-widget widget-user">
+           
+            <div class="widget-user-header designations bg-red">
+              <h3 class="widget-user-username"><b>USA</b></h3>
+              <h5 class="widget-user-desc"><i class="fa fa-map"></i> Address : GOR - 1, Golf Road, Lahore</h5>
+            </div>
+            
+             
+          </div>
+      
+        </div>
+        
+       
+      </div>
+     
+      
+    </section>
+ 
+  </div>
+<?php include("includes/footer.php");  ?>
