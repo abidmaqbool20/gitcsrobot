7a5707376b91c8135f9883bd5d837605db8a3223
abidmaqbool20 <abidmaqbool20@gmail.com>
2017-12-20 23:22:36 +0500

    functional page updated
    functional page updated

--- functional.php
+++ functional.php
@@ -90,7 +90,7 @@ padding-left: 20px;
 .tab-content-filter {
   padding: 0 20px 10px 0 !important
 }
-
+<<<<<<< HEAD
 /*-- Media Queries start Here  -- */
 
 @media only screen and (min-device-width: 1024px) 
@@ -185,7 +185,7 @@ padding-left: 20px;
         width: 100% !important;
        } 
   }
-
+=======
 @media only screen 
   and (min-device-width: 768px) 
   and (max-device-width: 1024px)  {
@@ -208,7 +208,7 @@ padding-left: 0;
  
 
 } 
-
+>>>>>>> e8b78edb99d5ff30d09c64ec0849906aeead7a75
  </style>
    
     <ul class="nav nav-pills">
@@ -216,21 +216,52 @@ padding-left: 0;
       <li><a data-toggle="tab" href="#2">About us</a></li>
       <li><a data-toggle="tab" href="#3">Contact us</a></li>
     </ul>
- 
+   <!--  <section class="action_header_info" style="">
+      <div class="row rowfilter">
+        <div class="col-md-5 col-sm-5 padlft0 col-xs-12">
+          <form>
+            <div class="form-group padlft0">
+              <select class="form-control select2" >
+                <option selected="selected">Alabama</option>
+                <option>Alaska</option>
+                <option>California</option>
+                <option>Delaware</option>
+                <option>Tennessee</option>
+                <option>Texas</option>
+                <option>Washington</option>
+              </select>
+          </div>
+          </form>
+        </div>
+        <div class="col-md-7 col-sm-7 col-xs-12 padlft0 ">
+          <div class="row rowfilterght">
+            <div class="col-md-3 col-md-push-3"></div>
+            <div class="col-md-3 col-sm-6 col-xs-12">
+              <form>
+                <div class="form-group">
+                  <select class="form-control">
+                    <option selected="selected">All Data</option>
+                    <option>Alaska</option>
+                    <option>California</option>
+                    <option>Delaware</option>
+                    <option>Tennessee</option>
+                    <option>Texas</option>
+                    <option>Washington</option>
+                  </select>
+                </div>
+              </form>
+            </div>
+            <div class="col-md-3 col-sm-4 padlft0 col-xs-3 "><button class="btn btn-success"><i class="fa fa-plus"></i>&nbsp; Add New</button></div>
+            <div class="col-md-2 col-sm-2 padlft0 col-xs-8"><button class="filteri" type="button" data-toggle="collapse" data-target="#filter-panel"><i class="fa fa-filter " aria-hidden="true"></i></button></div>
+          </div>
+        </div>
+      </div>
+    </section>
+     -->
     <section class="action_header_info" style="">
         <div class="row">
- 
-
-            <div class="col-md-12 col-sm-12 col-xs-12 padlft0" style="padding-left: 0px;">
-              <div class="col-md-2 col-sm-5 col-xs-12 pull-left padlft0" >
-
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padlft0" style="  padding-left: 0px;">
               <div class="col-lg-2 col-md-2 col-sm-4 col-xs-12 pull-left padlft0" >
-
- 
-            <div class="col-md-12 col-sm-12 col-xs-12 padlft0" style="margin-left: -20px; padding-left: 0px;">
-              <div class="col-md-2 col-sm-5 col-xs-12 pull-left padlft0" >
- 
                 <div class="form-group padlft0">
                     <div class="form-group padlft0">
                       <select class="form-control select2 " >
@@ -243,10 +274,10 @@ padding-left: 0;
               </div>
             </div>
 
-            <div class="col-md-offset-5 col-md-5 col-sm-7 col-xs-12 pull-right padlft0"  style="padding-left: 0px;">
-              <div class="col-md-offset-3 col-md-9 col-sm-12 col-xs-12 pull-right padlft0" style="padding-left: 0px; padding-right: 0px;">
+            <div class="col-lg-offset-2 col-lg-8 col-md-offset-2 col-md-8 col-sm-8 col-xs-12 pull-right padlft0"  style="padding-left: 0px;">
+              <div class=" col-lg-offset-4 col-md-offset-2 col-lg-8 col-md-10 col-sm-12 col-xs-12 pull-right padlft0" style="padding-left: 0px; padding-right: 0px;">
                 
-                <div class="col-md-4 col-sm-6 col-xs-12 pull-left padlft0"  style="padding-left: 0px;">
+                <div class="col-lg-4 col-md-4 col-sm-5 col-xs-12 pull-left padlft0"  style="padding-left: 0px;">
                    <div class="form-group padlft0"  style="padding-left: 0px;">
                      <div class="form-group padlft0">
                       <select class="form-control select2  " >
@@ -258,15 +289,19 @@ padding-left: 0;
                     </div>
                    </div>
                 </div>
-                 <div class="col-md-3 col-sm-4 col-xs-10 pull-left padlft0"  style="padding-left: 0px;">
+                 <div class="col-lg-2 col-md-3 col-sm-3 col-xs-8 pull-left padlft0"  style="padding-left: 0px;">
                   <button class="btn btn-success"><i class="fa fa-plus"></i>&nbsp; Add New</button>
                 </div>
                 
-                <div class="col-md-2 col-sm-2 col-xs-2 pull-left"  >
+                <div class="col-lg-2 col-md-3 col-sm-4 col-xs-2" style="text-align: center;"  >
                   <button class="filteri" type="button" data-toggle="collapse" data-target="#filter-panel" style="padding-left: 0px;">
                     <i class="fa fa-filter " aria-hidden="true"></i>
                   </button>
+                  <button class="filteri" type="button" data-toggle="collapse" data-target="#filter-panel" style="padding-left: 0px;">
+                    <i class="fa fa-ellipsis-h" aria-hidden="true"></i>
+                  </button>
                 </div>
+               
                
               </div>
                
@@ -311,7 +346,7 @@ padding-left: 0;
 
                               </tr>
                               <tr id="filter-panel" class="collapse filter-panel"  style="background-color: #eeefff;">
-                                <th colspan="4"><button>Search</button></th>
+                                <th colspan="4"></th>
                                
 
                                <th><input type="text" class="form-control" name=""></th>
